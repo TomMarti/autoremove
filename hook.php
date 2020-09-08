358e43a01e9c15754278f117dc183435bd815b22
TomMarti <tom@marti.wf>
2020-09-08 11:19:23 +0200

    after many many time it's work

--- hook.php
+++ hook.php
@@ -1,39 +1,69 @@
 <?php
-function plugin_autoremove_install() {
-    global $DB;
 
-    $migration = new Migration(100);
+/**
+ * Install hook
+ *
+ * @return boolean
+ */
+function plugin_autoremove_install()
+{
+    //do some stuff like instanciating databases, default values, ...
+    createADbTable("set_global_scheduler_glpi_plugin_autoremove",
+        "SET GLOBAL event_scheduler = ON;");
 
-    // Création de la table uniquement lors de la première installation
-    if (!TableExists("glpi_plugin_autoremove_profiles")) {
+    createADbTable("glpi_plugin_autoremove_remove", "CREATE EVENT glpi_plugin_autoremove_remove
+    ON SCHEDULE EVERY 1440 MINUTE DO
+        UPDATE glpi_softwares
+        SET is_deleted = 1
+        WHERE glpi_softwares.id IN (SELECT id FROM (SELECT DISTINCT `glpi_softwares`.`id` AS id, 'glpi' AS currentuser, `glpi_softwares`.`entities_id`, `glpi_softwares`.`is_recursive`, `glpi_softwares`.`name` AS `ITEM_Software_1`, `glpi_softwares`.`id` AS `ITEM_Software_1_id`, `glpi_manufacturers`.`name` AS `ITEM_Software_23`, GROUP_CONCAT(DISTINCT CONCAT(IFNULL(`glpi_softwareversions`.`name`, '__NULL__'), '$#$',`glpi_softwareversions`.`id`)
+        ORDER BY `glpi_operatingsystems_0a35c270152be19b5c8a485502badcd7`.`id` SEPARATOR '$$##$$') AS `ITEM_Software_4`, COUNT(DISTINCT `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf`.`id`) AS `NUMBERUSE`, FLOOR(SUM(`glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`number`) * COUNT(DISTINCT `glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`id`) / COUNT(`glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`id`)) AS `ITEM_Software_163`, MIN(`glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`number`) AS `ITEM_Software_163_min`
+        FROM `glpi_softwares`
+        LEFT JOIN `glpi_manufacturers` ON (`glpi_softwares`.`manufacturers_id` = `glpi_manufacturers`.`id` )
+        LEFT JOIN `glpi_softwareversions` ON (`glpi_softwares`.`id` = `glpi_softwareversions`.`softwares_id` )
+        LEFT JOIN `glpi_operatingsystems` AS `glpi_operatingsystems_0a35c270152be19b5c8a485502badcd7` ON (`glpi_softwareversions`.`operatingsystems_id` = `glpi_operatingsystems_0a35c270152be19b5c8a485502badcd7`.`id` )
+        LEFT JOIN `glpi_computers_softwareversions` AS `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf` ON (`glpi_softwareversions`.`id` = `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf`.`softwareversions_id` AND `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf`.`is_deleted_computer` = 0 AND `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf`.`is_deleted` = 0 AND `glpi_computers_softwareversions_dfb0ea52269e2c3215f034c26bee9fbf`.`is_template_computer` = 0 )
+        LEFT JOIN `glpi_softwarelicenses` AS `glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619` ON (`glpi_softwares`.`id` = `glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`softwares_id` AND `glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`is_template` = 0 AND (`glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`expire` IS NULL OR `glpi_softwarelicenses_daf59b6b5fae84097745ab089c081619`.`expire` > NOW()) )
+        WHERE `glpi_softwares`.`is_deleted` = 0 AND `glpi_softwares`.`is_template` = 0 GROUP BY `glpi_softwares`.`id`
+        ORDER BY NUMBERUSE ASC LIMIT 0, 5000) sub WHERE NUMBERUSE = 0)");
 
-        // requete de création de la table
-        $query = "CREATE TABLE `glpi_plugin_autoremove_profiles` (
-               `id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_profiles (id)',
-               `right` char(1) collate utf8_unicode_ci default NULL,
-               PRIMARY KEY  (`id`)
-             ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
-
-        $DB->queryOrDie($query, $DB->error());
-
-        $migration->executeMigration();
-
-        //creation du premier accès nécessaire lors de l'installation du plugin
-        include_once(GLPI_ROOT."/plugins/autoremove/inc/profile.php");
-        PluginAutoremoveProfile::createAdminAccess($_SESSION['glpiactiveprofile']['id']);
-    }
 
     return true;
 }
 
-function plugin_autoremove_uninstall() {
+/**
+ * Uninstall hook
+ *
+ * @return boolean
+ */
+function plugin_autoremove_uninstall()
+{
+    //to some stuff, like removing tables, generated files, ...
     global $DB;
 
-    $tables = array("glpi_plugin_autoremove_profiles");
+    $tablename = 'glpi_plugin_autoremove_remove';
+    //Create table only if it does not exists yet!
 
-    foreach($tables as $table) {
-        $DB->query("DROP TABLE IF EXISTS `$table`;");
-    }
+    $DB->queryOrDie(
+        "DROP EVENT `$tablename`",
+        $DB->error()
+    );
 
     return true;
+}
+
+function createADbTable ($name, $request) {
+    global $DB;
+
+    //instanciate migration with version
+    $migration = new Migration(100);
+
+    //Create table only if it does not exists yet!
+    if (!$DB->tableExists($name)) {
+        //table creation query
+        $query = $request;
+        $DB->queryOrDie($query, $DB->error());
+    }
+
+    //execute the whole migration
+    $migration->executeMigration();
 }
--- setup.php
+++ setup.php
@@ -1,38 +1,66 @@
 <?php
-function plugin_version_autoremove() {
-    return array(
-        'name'           => "Autoremove",
-        'version'        => '0.0.1',
-        'author'         => 'Tom Marti',
-        'license'        => 'GPLv3',
-        'homepage'       => 'https://github.com/TomMarti/autoremove',
-        'minGlpiVersion' => '0.84'
-    );// For compatibility / no install in version < 0.80
+
+define('AUTOREMOVE_VERSION', '0.1');
+
+/**
+ * Init the hooks of the plugins - Needed
+ *
+ * @return void
+ */
+function plugin_init_autoremove() {
+    global $PLUGIN_HOOKS;
+
+    //required!
+    $PLUGIN_HOOKS['csrf_compliant']['autoremove'] = true;
+
+    //some code here, like call to Plugin::registerClass(), populating PLUGIN_HOOKS, ...
 }
 
+/**
+ * Get the name and the version of the plugin - Needed
+ *
+ * @return array
+ */
+function plugin_version_autoremove() {
+    return [
+        'name'           => 'Autoremove',
+        'version'        => AUTOREMOVE_VERSION,
+        'author'         => 'Tom Marti',
+        'license'        => 'GLPv3',
+        'homepage'       => 'https://github.com/TomMarti/autoremove',
+        'requirements'   => [
+            'glpi'   => [
+                'min' => '9.1'
+            ]
+        ]
+    ];
+}
+
+/**
+ * Optional : check prerequisites before install : may print errors or add to message after redirect
+ *
+ * @return boolean
+ */
 function plugin_autoremove_check_prerequisites() {
-    if (version_compare(GLPI_VERSION,'0.84','lt') || version_compare(GLPI_VERSION,'0.85','gt')) {
-        echo "This plugin requires GLPI >= 0.84 and GLPI < 0.85";
-        return false;
-    }
+    //do what the checks you want
     return true;
 }
 
-function plugin_autoremove_check_config($verbose=false) {
+/**
+ * Check configuration process for plugin : need to return true if succeeded
+ * Can display a message only if failure and $verbose is true
+ *
+ * @param boolean $verbose Enable verbosity. Default to false
+ *
+ * @return boolean
+ */
+function plugin_autoremove_check_config($verbose = false) {
     if (true) { // Your configuration check
         return true;
     }
 
     if ($verbose) {
-        echo 'Installed / not configured';
+        echo "Installed, but not configured";
     }
     return false;
-}
-
-function plugin_init_autoremove() {
-    global $PLUGIN_HOOKS;
-
-    $PLUGIN_HOOKS['csrf_compliant']['autoremove'] = true;
-    Plugin::registerClass('PluginAutoremoveProfile');
-    Plugin::registerClass('PluginAutoremoveProfile', array('addtabon' => array('Profile')));
 }
